72061b4779b2d5953fa00ad45a4ae8d0598ea3c8
John Fairhurst <johnfairh@gmail.com>
2019-12-06 13:55:47 +0000

    Invoke Ruby

--- Sources/J2Lib/Wrapper.swift
+++ Sources/J2Lib/Wrapper.swift
@@ -10,6 +10,16 @@ import RubyGateway
 
 public enum Main {
     public static func run() {
-        print("Main.run, Ruby is: \(Ruby.version)")
+        do {
+            try Ruby.require(filename: "jazzy")
+            let jazzyConfigModule = try Ruby.get("Jazzy::Config")
+            let config = try jazzyConfigModule.call("parse!")
+            try jazzyConfigModule.setAttribute("instance", newValue: config)
+
+            let jazzyDocBuilderModule = try Ruby.get("Jazzy::DocBuilder")
+            try jazzyDocBuilderModule.call("build", args: [config])
+        } catch {
+            print("Ruby failure: \(error)")
+        }
     }
 }
