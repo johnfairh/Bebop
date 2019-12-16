37e78621b6abb2202e89e659b82301e5a1d4315f
John Fairhurst <johnfairh@gmail.com>
2019-12-16 20:43:40 +0000

    Land theme work

---
+++ Fw2020/dist/fw2020.js
@@ -0,0 +1,10 @@
+// J2 FW2020 theme.
+//
+// Distributed under the MIT license, https://github.com/johnfairh/J2/blob/master/LICENSE
+//
+$(function() {
+    $("#navToggleButton").click(function() {
+        const $nav = $("#navColumn")
+        $nav.toggleClass("d-none")
+    });
+});
