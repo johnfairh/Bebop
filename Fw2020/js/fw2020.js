c907829c5f5351e43e1e9a3d48aa3314439e1d99
John Fairhurst <johnfairh@gmail.com>
2019-12-28 13:50:48 +0000

    Initial prism.js integration

--- Fw2020/js/fw2020.js
+++ Fw2020/js/fw2020.js
@@ -8,3 +8,7 @@ $(function() {
         $nav.toggleClass("d-none")
     });
 });
+
+Prism.languages.swift['builtin'] = /\b(?:[A-Z]\S*)\b/;
+
+Prism.plugins.autoloader['languages_path'] = "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/";
