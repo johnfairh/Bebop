75762bac8151892a077a3934f2efb5f50e31d34b
John Fairhurst <johnfairh@gmail.com>
2019-12-29 13:20:09 +0000

    Fix up prism Swift grammar

--- Fw2020/js/fw2020.js
+++ Fw2020/js/fw2020.js
@@ -9,7 +9,28 @@ $(function() {
     });
 });
 
-Prism.languages.swift['builtin'] = /\b(?:[A-Z]\S*)\b/;
+Prism.languages.swift['keyword'] = [
+    /\b(?:as|Any|assignment|associatedtype|associativity|break|case|catch|class|continue|convenience|default|defer|deinit|didSet|do|dynamic|else|enum|extension|fallthrough|false|fileprivate|final|for|func|get|guard|higherThan|if|import|in|indirect|infix|init|inout|internal|is|lazy|left|let|lowerThan|mutating|nil|none|nonmutating|open|operator|optional|override|postfix|precedencegroup|prefix|private|protocol|public|repeat|required|rethrows|return|right|safe|self|Self|set|some|static|struct|subscript|super|switch|throws?|true|try|Type|typealias|unowned|unsafe|var|weak|where|while|willSet)\b/,
+    /#(?:available|colorLiteral|column|fileLiteral|function|imageLiteral|line|selector|sourceLocation)/,
+    /@\w+/,
+    /\$\d+/
+];
+
+Prism.languages.swift['tag'] = /#(?:else|elseif|endif|error|if|warning)/;
+
+Prism.languages.swift['punctuation'] = [ /[{}[\]().,:;=@&?!\\]/, /->/ ];
+
+Prism.languages.swift['class-name'] = {
+    pattern: /(\b(?:associatedtype|class|enum|extension|func|let|operator|protocol|precendencegroup|struct|typealias|var)\s+)`?\p{L}[\p{L}_\p{N}]*`?/u,
+    lookbehind: true
+};
+
+Prism.languages.swift['builtin'] = /\b\p{Lu}[\p{L}_\p{N}]*/u;
+
+delete Prism.languages.swift['boolean'];
+delete Prism.languages.swift['constant'];
+delete Prism.languages.swift['atrule'];
+delete Prism.languages.swift['function'];
 
 Prism.plugins.customClass.map((className, language) => {
     return 'pr-'+className;
