3852313d586a01c45921477ec969e9928a3cbc99
John Fairhurst <johnfairh@gmail.com>
2020-01-11 19:14:15 +0000

    Move prism js patches to a separate file

--- Fw2020/js/fw2020.js
+++ Fw2020/js/fw2020.js
@@ -25,6 +25,18 @@ if (isDashMode) {
   $('html').addClass('dash')
 }
 
+/*
+ * Prism customization for CSS tag names.
+ */
+Prism.plugins.customClass.map((className, language) => {
+  return 'pr-' + className
+})
+
+/*
+ * Prism customization for autoloading missing languages.
+ */
+Prism.plugins.autoloader.languages_path = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/'
+
 //
 // Narrow-mode nav collapse
 //
@@ -316,11 +328,39 @@ const searchControl = {
   }
 }
 
+//
+// Keypress handler
+//
+const keysControl = {
+  setup () {
+    $(document).keydown((e) => this.keydown(e))
+  },
+
+  keydown (e) {
+    const $searchField = $('input:visible')
+
+    if ($searchField.is(':focus')) {
+      if (e.key === 'Escape') {
+        $searchField.blur()
+        $searchField.typeahead('val', '')
+      }
+    } else {
+      switch (e.key) {
+        case '/': $searchField.focus(); return false
+        case 'a': collapseControl.toggle(); break
+        case 'l': langControl.toggle(); break
+        case 'd': $('#size-debug').toggleClass('d-flex d-none'); break
+      }
+    }
+  }
+}
+
 if (!isDashMode) {
   navControl.setup()
   langControl.setup()
   collapseControl.setup()
   searchControl.setup()
+  keysControl.setup()
 }
 
 $(function () {
@@ -345,78 +385,3 @@ $(function () {
   // Typeahead
   searchControl.ready()
 })
-
-// Keypress handler
-//
-$(document).keydown(function (e) {
-  if (isDashMode) return
-
-  const $searchField = $('input:visible')
-
-  if ($searchField.is(':focus')) {
-    if (e.key === 'Escape') {
-      $searchField.blur()
-      $searchField.typeahead('val', '')
-    }
-  } else {
-    switch (e.key) {
-      case '/': $searchField.focus(); return false
-      case 'a': collapseControl.toggle(); break
-      case 'l': langControl.toggle(); break
-      case 'd': $('#size-debug').toggleClass('d-flex d-none'); break
-    }
-  }
-})
-
-/*
- * Prism customization for Swift highlighting.
- * Some of this should go upstream.
- */
-Prism.languages.swift.keyword = [
-  /\b(?:as|Any|assignment|associatedtype|associativity|break|case|catch|class|continue|convenience|default|defer|deinit|didSet|do|dynamic|else|enum|extension|fallthrough|false|fileprivate|final|for|func|get|guard|higherThan|if|import|in|indirect|infix|init|inout|internal|is|lazy|left|let|lowerThan|mutating|nil|none|nonmutating|open|operator|optional|override|postfix|precedencegroup|prefix|private|protocol|public|repeat|required|rethrows|return|right|safe|self|Self|set|some|static|struct|subscript|super|switch|throws?|true|try|Type|typealias|unowned|unsafe|var|weak|where|while|willSet)\b/,
-  /#(?:available|colorLiteral|column|fileLiteral|function|imageLiteral|line|selector|sourceLocation)/,
-  /@\w+/,
-  /\$\d+/
-]
-
-Prism.languages.insertBefore('swift', 'function', {
-  tag: /#(?:else|elseif|endif|error|if|warning)/
-})
-
-// Color type declarations.  The id regexps are barely approximate.
-Prism.languages.swift['class-name'] = {
-  pattern: /(\b(?:associatedtype|class|enum|extension|func|let|operator|protocol|precedencegroup|struct|typealias|var)\s+)`?[_\p{L}][\p{L}_\p{N}.]*`?/u,
-  lookbehind: true
-}
-
-// Color (probable) type refs
-Prism.languages.swift.builtin = /\b\p{Lu}[\p{L}_\p{N}]*/u
-
-delete Prism.languages.swift.boolean
-delete Prism.languages.swift.constant
-delete Prism.languages.swift.atrule
-
-/*
- * Prism customization for Objective-C highlighting.
- * This is only to add property attributes.
- */
-Prism.languages.objectivec.keyword = [
-  /\b(?:asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|in|self|super)\b|(?:@interface|@end|@implementation|@protocol|@class|@public|@protected|@private|@property|@try|@catch|@finally|@throw|@synthesize|@dynamic|@selector)\b/,
-  /\b(?:(non)?atomic|readonly|readwrite|strong|weak|assign|copy)\b/,
-  /\b(?:getter=|setter=)/
-]
-
-// Add a bit more color
-Prism.languages.objectivec.builtin = /\b[A-Z][\w0-9_]*/
-
-/*
- * Prism customization for CSS tag names.
- */
-Prism.plugins.customClass.map((className, language) => {
-  return 'pr-' + className
-})
-
-/*
- * Prism customization for autoloading missing languages.
- */
-Prism.plugins.autoloader.languages_path = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/'
---
+++ Fw2020/js/prism-extras.js
@@ -0,0 +1,50 @@
+// J2 FW2020 theme.
+//
+// Distributed under the MIT license
+// https://github.com/johnfairh/J2/blob/master/LICENSE
+//
+
+/* global Prism */
+
+'use strict'
+
+/*
+ * Prism customization for Swift highlighting.
+ * Some of this should go upstream.
+ */
+Prism.languages.swift.keyword = [
+  /\b(?:as|Any|assignment|associatedtype|associativity|break|case|catch|class|continue|convenience|default|defer|deinit|didSet|do|dynamic|else|enum|extension|fallthrough|false|fileprivate|final|for|func|get|guard|higherThan|if|import|in|indirect|infix|init|inout|internal|is|lazy|left|let|lowerThan|mutating|nil|none|nonmutating|open|operator|optional|override|postfix|precedencegroup|prefix|private|protocol|public|repeat|required|rethrows|return|right|safe|self|Self|set|some|static|struct|subscript|super|switch|throws?|true|try|Type|typealias|unowned|unsafe|var|weak|where|while|willSet)\b/,
+  /#(?:available|colorLiteral|column|fileLiteral|function|imageLiteral|line|selector|sourceLocation)/,
+  /@\w+/,
+  /\$\d+/
+]
+
+Prism.languages.insertBefore('swift', 'function', {
+  tag: /#(?:else|elseif|endif|error|if|warning)/
+})
+
+// Color type declarations.  The id regexps are barely approximate.
+Prism.languages.swift['class-name'] = {
+  pattern: /(\b(?:associatedtype|class|enum|extension|func|let|operator|protocol|precedencegroup|struct|typealias|var)\s+)`?[_\p{L}][\p{L}_\p{N}.]*`?/u,
+  lookbehind: true
+}
+
+// Color (probable) type refs
+Prism.languages.swift.builtin = /\b\p{Lu}[\p{L}_\p{N}]*/u
+
+delete Prism.languages.swift.boolean
+delete Prism.languages.swift.constant
+delete Prism.languages.swift.atrule
+
+/*
+ * Prism customization for Objective-C highlighting.
+ * This is only to add property attributes.
+ */
+Prism.languages.objectivec.keyword = [
+  /\b(?:asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while|in|self|super)\b|(?:@interface|@end|@implementation|@protocol|@class|@public|@protected|@private|@property|@try|@catch|@finally|@throw|@synthesize|@dynamic|@selector)\b/,
+  /\b(?:(non)?atomic|readonly|readwrite|strong|weak|assign|copy)\b/,
+  /\b(?:getter=|setter=)/
+]
+
+// Add a bit more color
+Prism.languages.objectivec.builtin = /\b[A-Z][\w0-9_]*/
