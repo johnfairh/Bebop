1d8ef70f0a623be4ff22d0a4c0c6789b10a49085
John Fairhurst <johnfairh@gmail.com>
2020-02-13 16:38:41 +0000

    Always assign topics

--- Sources/J2Lib/Merge/Group/Group.swift
+++ Sources/J2Lib/Merge/Group/Group.swift
@@ -43,3 +43,34 @@ public struct Group: Configurable {
         return kindGroups
     }
 }
+
+extension Item {
+    /// Sort out the topics of items to get rid of dups or weird gaps and make sure everything
+    /// has a topic.  Probably only needed when the topics come from MARK comments in jazzy mode.
+    func rationalizeTopics() {
+        guard let firstChild = children.first else {
+            return
+        }
+        // Start an empty topic if there is none
+        var currentTopic = firstChild.topic ?? Topic()
+        children.forEach { child in
+            child.rationalizeTopics()
+            guard let childTopic = child.topic else {
+                // add to current topic
+                child.topic = currentTopic
+                return
+            }
+            if childTopic === currentTopic {
+                // already there
+                return
+            }
+            if childTopic == currentTopic {
+                // textual dup (different file origin/sorting artefact?), merge
+                child.topic = currentTopic
+                return
+            }
+            // New topic!
+            currentTopic = childTopic
+        }
+    }
+}
