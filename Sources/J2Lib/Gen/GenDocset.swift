e53e69e5c8b645aebdffa702a64f26e42727f790
John Fairhurst <johnfairh@gmail.com>
2020-04-18 13:09:23 +0100

    Docset - module-name setting

--- Sources/J2Lib/Gen/GenDocset.swift
+++ Sources/J2Lib/Gen/GenDocset.swift
@@ -11,8 +11,19 @@ import SQLite
 
 public final class GenDocset: Configurable {
     let docsetPathOpt = PathOpt(l: "docset-path").help("PATH").hidden
+    let docsetModuleNameOpt = StringOpt(l: "docset-module-name").help("MODULENAME")
+    let published: Published
+
+    /// Module Name to use for the docset - defaults to one of the source modules
+    var moduleName: String {
+        if let docsetModuleName = docsetModuleNameOpt.value {
+            return docsetModuleName
+        }
+        return published.moduleNames.first!
+    }
 
     init(config: Config) {
+        published = config.published
         config.register(self)
     }
 
