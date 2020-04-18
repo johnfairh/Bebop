f26da096da83c2965e5c7e55ba164ad3bdd87260
John Fairhurst <johnfairh@gmail.com>
2020-04-18 13:36:34 +0100

    Docset - filecopy, test update

--- Sources/J2Lib/Gen/GenDocset.swift
+++ Sources/J2Lib/Gen/GenDocset.swift
@@ -33,7 +33,38 @@ public final class GenDocset: Configurable {
         }
     }
 
-    func generate(outputURL: URL, items: [Item]) throws {
+    static let DOCSET_TOP = "docsets"
+    static let DOCSET_SUFFIX = ".docset"
 
+    func generate(outputURL: URL, items: [Item]) throws {
+        let docsetTopURL = outputURL.appendingPathComponent(Self.DOCSET_TOP)
+        let docsetDirURL = docsetTopURL.appendingPathComponent("\(moduleName)\(Self.DOCSET_SUFFIX)")
+        logDebug("Docset: cleaning any old content")
+        try? FileManager.default.removeItem(at: docsetTopURL)
+
+        try copyDocs(outputURL: outputURL, docsetDirURL: docsetDirURL)
+    }
+
+    /// Copy over the files
+    func copyDocs(outputURL: URL, docsetDirURL: URL) throws {
+        let docsTargetURL = docsetDirURL
+            .appendingPathComponent("Contents")
+            .appendingPathComponent("Resources")
+            .appendingPathComponent("Documents")
+
+        logDebug("Docset: copying up docs")
+
+        try FileManager.default.createDirectory(atPath: docsTargetURL.path, withIntermediateDirectories: true)
+
+        // Exclude ourselves and any secondary localizations
+        let skipFiles = Set([Self.DOCSET_TOP] + Localizations.shared.others.map(\.tag))
+
+        try Glob.files(.init(outputURL.appendingPathComponent("*").path)).forEach { sourceURL in
+            let filename = sourceURL.lastPathComponent
+            if !skipFiles.contains(filename) {
+                let targetURL = docsTargetURL.appendingPathComponent(filename)
+                try FileManager.default.copyItem(at: sourceURL, to: targetURL)
+            }
+        }
     }
 }
