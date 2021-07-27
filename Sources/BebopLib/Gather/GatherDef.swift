c9cbcc66ea9811f117a16939aaf9b6c3c804588b
John Fairhurst <johnfairh@gmail.com>
2021-07-27 12:26:08 +0100

    Correct 'actor' prefix in index pages

--- Sources/BebopLib/Gather/GatherDef.swift
+++ Sources/BebopLib/Gather/GatherDef.swift
@@ -200,10 +200,9 @@ extension DefKind {
                     return SwiftDeclarationKind2.functionSubscriptClass
                 }
             }
-            if isSwiftClass, let annotatedDecl = dict.fullyAnnotatedDecl,
-               annotatedDecl.contains("<syntaxtype.keyword>actor") {
-                return SwiftDeclarationKind2.actor
-            }
+        } else if isSwiftClass, let annotatedDecl = dict.fullyAnnotatedDecl,
+                  annotatedDecl.contains("<syntaxtype.keyword>actor") {
+            return SwiftDeclarationKind2.actor
         }
         return nil
     }
